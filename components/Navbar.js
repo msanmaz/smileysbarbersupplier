5c4ec9777c3c110800e21aab898086b20bc363f5
msanmaz <osanmaz4992@gmail.com>
2021-03-14 02:53:23 +0000

    vercel1

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,10 @@
 
 import React, { useCallback, useState } from 'react';
-import useDarkMode from '../hook/useDarkMode';
 import Link from 'next/link'
 import Hamburger from 'hamburger-react'
 
 
 export default function Navbar() {
-    const [colorTheme, setTheme] = useDarkMode();
     const [isOpen, setIsOpen] = React.useState(false);
     const [showMenu, setShowMenu] = React.useState(false);
     React.useLayoutEffect = React.useEffect
@@ -134,20 +132,6 @@ export default function Navbar() {
                 </div>
 
 
-                <span onClick={() => setTheme(colorTheme)} className="w-10 h-10 bg-black block rounded full shadow-lg cursor-pointer text-white flex items-center justify-center" >
-                    {colorTheme === "light" ?
-                        <svg className="w-6 h-6" fill="none" stroke="white"
-                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path>
-                        </svg> :
-                        <svg className="w-6 h-6" fill="none" stroke="currentColor"
-                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                strokeWidth="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path>
-                        </svg>}
-
-                </span>
 
 
                 <button

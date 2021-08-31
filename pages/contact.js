cba5d7b60b44ef5d1e57b894472e04feb8dd285e
msanmaz <osanmaz4992@gmail.com>
2021-08-31 15:53:27 +0100

    fuggy

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Layout from '../layout/layout'
-import GoogleMapReact from 'google-map-react';
 
 
 const Contact = () => {
@@ -148,19 +147,7 @@ const Contact = () => {
 
                 </div>
 
-                <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "721119149079-o75t8nsu0ofdcjk81ld4l1jslpe76f8f.apps.googleusercontent.com"}}
-          defaultCenter={"lat:59.95","lng:30.33"}
-          defaultZoom={11}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
+
 
 
             </div>
